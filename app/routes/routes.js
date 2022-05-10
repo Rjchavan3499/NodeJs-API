b43f19c06625af582b0463beed562cadb076e3b3
Rjchavan3499 <59140623+Rjchavan3499@users.noreply.github.com>
2022-05-10 11:24:54 +0530

    Update routes.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,5 @@
 const controller = require('../controller/controller')
 
 module.exports = (routes) => {
-    routes.get('/hello_world  how are you i am fine ', controller.messageController);
+    routes.get('/hello_world', controller.messageController);
 }
